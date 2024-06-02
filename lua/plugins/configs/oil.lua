3704bb485becbef5c262db2f68f737d505714c92
David Jobe <daavidjobe@gmail.com>
2024-06-02 14:33:52 +0200

    Configured Oil. A directory editor

---
+++ lua/plugins/configs/oil.lua
@@ -0,0 +1,143 @@
+local M = {}
+
+M.setup = function()
+	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+	require("oil").setup({
+		default_file_explorer = true,
+		columns = {
+			"icon",
+			-- "permissions",
+			-- "size",
+			-- "mtime",
+		},
+		buf_options = {
+			buflisted = false,
+			bufhidden = "hide",
+		},
+		win_options = {
+			wrap = false,
+			signcolumn = "no",
+			cursorcolumn = false,
+			foldcolumn = "0",
+			spell = false,
+			list = false,
+			conceallevel = 3,
+			concealcursor = "nvic",
+		},
+		delete_to_trash = false,
+		skip_confirm_for_simple_edits = false,
+		prompt_save_on_select_new_entry = true,
+		cleanup_delay_ms = 2000,
+		lsp_file_methods = {
+			timeout_ms = 1000,
+			autosave_changes = false,
+		},
+		constrain_cursor = "editable",
+		experimental_watch_for_changes = false,
+		keymaps = {
+			["g?"] = "actions.show_help",
+			["<CR>"] = "actions.select",
+			["<C-s>"] = "actions.select_vsplit",
+			["<C-h>"] = "actions.select_split",
+			["<C-t>"] = "actions.select_tab",
+			["<C-p>"] = "actions.preview",
+			["<C-c>"] = "actions.close",
+			["<C-l>"] = "actions.refresh",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
+			["`"] = "actions.cd",
+			["~"] = "actions.tcd",
+			["gs"] = "actions.change_sort",
+			["gx"] = "actions.open_external",
+			["g."] = "actions.toggle_hidden",
+			["g\\"] = "actions.toggle_trash",
+		},
+		-- Set to false to disable all of the above keymaps
+		use_default_keymaps = true,
+		view_options = {
+			-- Show files and directories that start with "."
+			show_hidden = true,
+			-- This function defines what is considered a "hidden" file
+			is_hidden_file = function(name)
+				return vim.startswith(name, ".") or string.find(name, "node_modules")
+			end,
+			-- This function defines what will never be shown, even when `show_hidden` is set
+			is_always_hidden = function(name, bufnr)
+				return false
+			end,
+			-- Sort file names in a more intuitive order for humans. Is less performant,
+			-- so you may want to set to false if you work with large directories.
+			natural_order = true,
+			sort = {
+				-- sort order can be "asc" or "desc"
+				-- see :help oil-columns to see which columns are sortable
+				{ "type", "asc" },
+				{ "name", "asc" },
+			},
+		},
+		-- Extra arguments to pass to SCP when moving/copying files over SSH
+		extra_scp_args = {},
+		-- EXPERIMENTAL support for performing file operations with git
+		git = {
+			-- Return true to automatically git add/mv/rm files
+			add = function(path)
+				return false
+			end,
+			mv = function(src_path, dest_path)
+				return false
+			end,
+			rm = function(path)
+				return false
+			end,
+		},
+		float = {
+			-- Padding around the floating window
+			padding = 2,
+			max_width = 0,
+			max_height = 0,
+			border = "rounded",
+			win_options = {
+				winblend = 0,
+			},
+			-- This is the config that will be passed to nvim_open_win.
+			-- Change values here to customize the layout
+			override = function(conf)
+				return conf
+			end,
+		},
+		preview = {
+			max_width = 0.9,
+			min_width = { 40, 0.4 },
+			width = nil,
+			max_height = 0.9,
+			min_height = { 5, 0.1 },
+			height = nil,
+			border = "rounded",
+			win_options = {
+				winblend = 0,
+			},
+			update_on_cursor_moved = true,
+		},
+		progress = {
+			max_width = 0.9,
+			min_width = { 40, 0.4 },
+			width = nil,
+			max_height = { 10, 0.9 },
+			min_height = { 5, 0.1 },
+			height = nil,
+			border = "rounded",
+			minimized_border = "none",
+			win_options = {
+				winblend = 0,
+			},
+		},
+		ssh = {
+			border = "rounded",
+		},
+		keymaps_help = {
+			border = "rounded",
+		},
+	})
+end
+return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,22 +10,15 @@
 --
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
-	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
-	-- NOTE: Plugins can also be added by using a table,
-	-- with the first argument being the link and the following
-	-- keys can be used to configure plugin behavior/loading/etc.
-	--
-	-- Use `opts = {}` to force a plugin to be loaded.
-	--
-	--  This is equivalent to:
-	--    require('Comment').setup({})
-
 	{
-		dir = "~/git/nvim-tree",
-		opts = {},
+		"stevearc/oil.nvim",
+		config = function()
+			require("plugins.configs.oil").setup()
+		end,
 	},
+
 	{
 		"rest-nvim/rest.nvim",
 		dependencies = { { "nvim-lua/plenary.nvim" } },
